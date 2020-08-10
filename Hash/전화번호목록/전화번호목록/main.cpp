e5717044231826a27cc2bdacf5d172fec7910514
DESKTOP-9L4KOLL\K <kore207@naver.com>
2020-08-10 22:31:59 +0900

    no message

---
+++ Hash/전화번호목록/전화번호목록/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+void main() {
+	vector<string> phone_book = { "119", "97674223", "1195524421" };
+
+	for (auto temp : phone_book) {
+
+	}
+
+}
